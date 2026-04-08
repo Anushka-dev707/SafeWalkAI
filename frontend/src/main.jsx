90490dd42124d81d233c0e8db89365e4bfe3c5ff
Anushka-dev707 <goyala1001@gmail.com>
2026-04-08 08:31:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,16 @@ import './index.css'
 import "leaflet/dist/leaflet.css";
 import App from './App.jsx'
 
+import L from "leaflet";
+
+// ✅ FIX Leaflet marker issue
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
